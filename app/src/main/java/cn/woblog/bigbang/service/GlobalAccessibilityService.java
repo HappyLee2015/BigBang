47e34a52810fea2109c5a001db6dbfef3714206e
lifengsofts <30234@163.com>
2016-10-29 10:40:46 +0800

    refactor code

--- app/src/main/java/cn/woblog/bigbang/service/GlobalAccessibilityService.java
+++ app/src/main/java/cn/woblog/bigbang/service/GlobalAccessibilityService.java
@@ -4,6 +4,7 @@ import android.accessibilityservice.AccessibilityService;
 import android.content.Intent;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.TextView;
 
 import cn.woblog.bigbang.activity.ParticipleActivity;
 import cn.woblog.bigbang.domain.Info;
@@ -12,26 +13,19 @@ import cn.woblog.bigbang.domain.Info;
  * Created by renpingqing on 10/29/16.
  */
 public class GlobalAccessibilityService extends AccessibilityService {
-    private CharSequence windowClass;
-
     @Override
     public void onAccessibilityEvent(AccessibilityEvent accessibilityEvent) {
         int eventType = accessibilityEvent.getEventType();
         CharSequence className = accessibilityEvent.getClassName();
         switch (eventType) {
-            case AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED: {
-                windowClass = accessibilityEvent.getClassName();
-                break;
-            }
             case AccessibilityEvent.TYPE_VIEW_CLICKED: {
-                if ("android.widget.TextView".equals(className)) {
+                if (TextView.class.getName().equals(className)) {
                     AccessibilityNodeInfo source = accessibilityEvent.getSource();
                     String text = source.getText().toString();
                     Intent intent = new Intent(this, ParticipleActivity.class);
                     intent.addFlags(intent.getFlags() | Intent.FLAG_ACTIVITY_NEW_TASK);
                     intent.putExtra(ParticipleActivity.DATA, new Info(Info.TYPE_TEXT, text.toString()));
                     startActivity(intent);
-
                 }
                 break;
             }
